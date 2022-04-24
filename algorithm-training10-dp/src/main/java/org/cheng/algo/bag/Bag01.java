cb119d63bced8bae0cc4ad4282e1e2c95a524799
chengfpvoid <cfpvoid@gmail.com>
2022-04-24 17:32:25 +0800

    update 2 dismission bag01 index

--- algorithm-training10-dp/src/main/java/org/cheng/algo/bag/Bag01.java
+++ algorithm-training10-dp/src/main/java/org/cheng/algo/bag/Bag01.java
@@ -13,23 +13,27 @@ public class Bag01 {
     public static void testWeightBagProblem(int[] weight, int[] value, int bagSize) {
         int wlen = weight.length, value0 = 0;
         //定义dp数组：dp[i][j]表示背包容量为j时，前i个物品能获得的最大价值
-        int[][] dp = new int[wlen + 1][bagSize + 1];
-        //初始化：背包容量为0时，能获得的价值都为0
-        for (int i = 0; i <= wlen; i++) {
-            dp[i][0] = value0;
+        int[][] dp = new int[wlen][bagSize + 1];
+        // 初始化 i 有 i-1 而来 首先dp[0][j] 需要初始化
+        for (int j = 0; j <= bagSize; j++) {
+            dp[0][j] = value[0];
         }
-        //遍历顺序：先遍历物品，再遍历背包容量
-        for (int i = 1; i <= wlen; i++) {
+
+        // 递推 遍历的顺序 i范围 0  到  物品个数 - 1
+        // 遍历顺序来自左上，对调下也是没问题的
+        for (int i = 1; i < wlen; i++) {
             for (int j = 1; j <= bagSize; j++) {
-                if (j < weight[i - 1]) {
-                    dp[i][j] = dp[i - 1][j];
+                if (j < weight[i]) {
+                    // 第i个物品放不进去
+                    dp[i][j] = dp[i-1][j];
                 } else {
-                    dp[i][j] = Math.max(dp[i - 1][j], dp[i - 1][j - weight[i - 1]] + value[i - 1]);
+                    dp[i][j] = Math.max(dp[i-1][j],dp[i-1][j - weight[i]] + value[i]);
                 }
             }
         }
+
         //打印dp数组
-        for (int i = 0; i <= wlen; i++) {
+        for (int i = 0; i < wlen; i++) {
             for (int j = 0; j <= bagSize; j++) {
                 System.out.print(dp[i][j] + " ");
             }
